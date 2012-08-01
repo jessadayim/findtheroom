f8e33e1e41b83c95aa088ab3f508412148a5bcb5
suriya <suriya257_@hotmail.com>
2012-08-01 11:48:48 +0700

    dol commit

--- src/FTR/WebBundle/Controller/MainController.php
+++ src/FTR/WebBundle/Controller/MainController.php
@@ -3,18 +3,18 @@
 namespace FTR\WebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Acme\WebBundle\Entity\Building_site;
+use Acme\WebBundle\Repository\Building_siteRepository;
 
 class MainController extends Controller
 {
-    public function testAction()
-	{
-		echo "test";
-		exit();
-	}
-	
+    
     public function indexAction()
     {
+    	$em = $this->getDoctrine()->getEntityManager();
+    	$building = $em->getRepository('FTRWebBundle:Building_site')->findall();
+		
+		var_dump($building);
         return $this->render('FTRWebBundle:Main:index.html.twig', array());
     }
 }
