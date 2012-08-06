6b1165d3ba1538e99579102011ee9dfe2833d00d
suriya <suriya257_@hotmail.com>
2012-08-06 18:15:28 +0700

    regis and login

--- src/FTR/WebBundle/Controller/MainController.php
+++ src/FTR/WebBundle/Controller/MainController.php
@@ -11,10 +11,49 @@ class MainController extends Controller
     
     public function indexAction()
     {
+    	$id = 1;
     	$em = $this->getDoctrine()->getEntityManager();
-    	$building = $em->getRepository('FTRWebBundle:Building_site')->findall();
 		
-		var_dump($building);
-        return $this->render('FTRWebBundle:Main:index.html.twig', array());
+		$conn= $this->get('database_connection');
+		if(!$conn){ die("MySQL Connection error");}
+			try{
+				$sql1 ="SELECT b.*,n.*,t.*,z.* FROM building_site b JOIN building_type t ON b.building_type_id = t.id
+									 JOIN zone z ON b.zone_id = z.id
+									 JOIN nearly2site n2 ON n2.building_site_id = b.id
+									 JOIN nearly_location n ON n.id = n2.nearly_location_id
+									 JOIN nearly_type nt ON nt.id = n.nearly_type_id
+						WHERE b.recommend = 0 and nt.id = 1";
+				$objSQL1 = $conn->fetchAll($sql1);
+		
+			} catch (Exception $e) {
+				echo 'Caught exception: ',  $e->getMessage(), "\n";
+			}
+			try{
+				$sql2 ="SELECT b.*,n.*,t.*,z.* FROM building_site b JOIN building_type t ON b.building_type_id = t.id
+									 JOIN zone z ON b.zone_id = z.id
+									 JOIN nearly2site n2 ON n2.building_site_id = b.id
+									 JOIN nearly_location n ON n.id = n2.nearly_location_id
+									 JOIN nearly_type nt ON nt.id = n.nearly_type_id
+						WHERE b.recommend = 0 and nt.id = 2";
+				$objSQL2 = $conn->fetchAll($sql2);
+		
+			} catch (Exception $e) {
+				echo 'Caught exception: ',  $e->getMessage(), "\n";
+			}
+			try{
+				$sql3 ="SELECT b.*,n.*,t.*,z.* FROM building_site b JOIN building_type t ON b.building_type_id = t.id
+									 JOIN zone z ON b.zone_id = z.id
+									 JOIN nearly2site n2 ON n2.building_site_id = b.id
+									 JOIN nearly_location n ON n.id = n2.nearly_location_id
+									 JOIN nearly_type nt ON nt.id = n.nearly_type_id
+						WHERE b.recommend = 0 and nt.id =3";
+				$objSQL3 = $conn->fetchAll($sql3);
+		
+			} catch (Exception $e) {
+				echo 'Caught exception: ',  $e->getMessage(), "\n";
+			}
+		//var_dump($objSQL1,$objSQL2,$objSQL3);
+		//exit();
+       return $this->render('FTRWebBundle:Main:index.html.twig',array('item1'=>$objSQL1,'item2'=>$objSQL2,'item3'=>$objSQL3));
     }
 }
