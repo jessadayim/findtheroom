1a4e4f3d120f93a59f603ba750c450f8a22a859b
Jessada Yimvilai <exodist@gmail.com>
2012-08-21 22:34:10 +0700

    clear uploadify cloud script

--- web/js/uploadify.control.js
+++ web/js/uploadify.control.js
@@ -21,21 +21,19 @@ $(function() {// get more knowledge at http://www.uploadify.com/documentation/
 $(function() {// get more knowledge at http://www.uploadify.com/documentation/
 		loopbutton();
 		
-		test();
+		//test();
 	});
 
-function test()
+function uploadpicroom()
 {
 	var ownername = document.getElementById('hdnownername').value;
 	var buildid = document.getElementById('hdnbuildid').value;
 	var numberline = parseInt(document.addform.hdnMaxLine.value);
-	alert(numverline)
-	for(i=0;i<numberline;i++){
+	for(i=0;i<=numberline;i++){
 	var name = '#room'+i
-	alert(name);
 	$(name).uploadify({
-			'formData'      : {'ownername' : ownername, 'buildid' : buildid ,'fieldname' : 'room'},
-			'debug'    : true,
+			'formData'      : {'ownername' : ownername, 'buildid' : buildid ,'fieldname' : name},
+			//'debug'    : true,
 			'swf'      : 'js/uploadify/uploadify.swf',
 			'uploader' : 'js/uploadify/uploadify.php',
 			//'uploadLimit' : 1,
@@ -45,8 +43,10 @@ function test()
 			'onFallback' : function() {alert('Flash was not detected.');},// detect flash compatible
 			'onUploadSuccess' : function(file, data, response) {
             	//alert('The file ' + file.name + ' was successfully uploaded with a response of ' + response + ':' + data);
-            	alert(myID)
-            	document.getElementById('hdnfilename0').value = data;
+            	var mySplitResult = data.split("_");
+            	var textroomname = 'hdnfilename'+mySplitResult[1];
+            	//alert(textroomname)
+            	document.getElementById(textroomname).value = mySplitResult[0];
         	}
 			// Your options here
 		});
--- web/js/uploadify/uploadify.php
+++ web/js/uploadify/uploadify.php
@@ -16,8 +16,13 @@ if (!empty($_FILES)) {
 	$fileTypes = array('jpg','jpeg','gif','png'); // File extensions
 	$fileParts = pathinfo($_FILES['Filedata']['name']);
 	
+	$newfieldname1 = str_replace('#', '', $fieldname);
+	$newfieldname2 = str_replace('#room', '', $fieldname);
+	
 	$fileextension = $fileParts['extension'];
-	$fixfilename = $now.$fieldname.'.'.$fileextension;
+	$fixfilename = $now.$newfieldname1.'.'.$fileextension;
+	
+	$filenamecallback = $newfieldname1.'.'.$fileextension.'_'.$newfieldname2;
 	
 	$tempFile = $_FILES['Filedata']['tmp_name'];
 	$targetPath = $_SERVER['DOCUMENT_ROOT'] . $targetFolder . '/' .$build_id;
@@ -26,7 +31,7 @@ if (!empty($_FILES)) {
 	if (in_array($fileParts['extension'],$fileTypes)) {
 		move_uploaded_file($tempFile,$targetFile);
 		//echo $targetFolder . '/' . $_FILES['Filedata']['name'];
-		echo $fixfilename;
+		echo $filenamecallback;
 		//echo '1';
 	} else {
 		echo 'Invalid file type.';
