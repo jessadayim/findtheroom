62e8c21e7de5595abaa130a55e093126c06baff5
Mr. Wararit Satitnimankarn <manesz13@gmail.com>
2012-08-28 13:38:39 +0700

    Insert New Panel Backend Template

---
+++ src/FTR/AdminBundle/Controller/PanelController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace FTR\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class PanelController extends Controller
+{
+    
+    public function indexAction()
+    {
+    	$request = $this->get('request');
+		
+		if($request->getMethod()=='POST'){
+			$username = $request->get('username');
+			$password = $request->get('password');
+		}
+		
+        return $this->render('FTRAdminBundle:Ftr_panel:signin.html.twig', array('name' => NULL));
+    }
+	
+}
