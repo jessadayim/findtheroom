a83bdf1795f0414dc21e38bdcc9c1ccba83c50f5
Jessada Yimvilai <exodist@gmail.com>
2012-09-03 06:09:21 +0700

    แก้การบันทึกชื่อการอัพรูปเล็กน้อย

--- web/js/uploadify/uploadify.php
+++ web/js/uploadify/uploadify.php
@@ -35,7 +35,7 @@ if (!empty($_FILES)) {
 	if (in_array($fileParts['extension'],$fileTypes)) {
 		move_uploaded_file($tempFile,$targetFile);
 		//echo $targetFolder . '/' . $_FILES['Filedata']['name'];
-		echo $filenamecallback;
+		echo $fixfilename;
 		//echo '1';
 	} else {
 		echo 'Invalid file type.';
