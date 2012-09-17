0c213fa5d30b73ac13f10d4c13fabc957f684df5
suriyajun <suriya257_@hotmail.com>
2012-09-17 12:05:45 +0700

    แก้ไข admin manage user และ หน้า detail

--- src/FTR/AdminBundle/Controller/AdminprofileController.php
+++ src/FTR/AdminBundle/Controller/AdminprofileController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use FTR\AdminBundle\Entity\User_admin;
 use FTR\AdminBundle\Form\User_adminType;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 
 class AdminprofileController extends Controller
@@ -16,63 +17,104 @@ class AdminprofileController extends Controller
      * Displays a form to edit an existing User_admin entity.
      *
      */
-    public function editAction() {
-        $id = $_GET['id'];
-        $em = $this -> getDoctrine() -> getEntityManager();
-        $entity = $em -> getRepository('FTRAdminBundle:User_admin') -> find($id);
+    public function editAction()
+    {
+        $session = $this->get('session');
+        $id = $session->get('id');
+        if (!empty($id)) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $entity = $em->getRepository('FTRAdminBundle:User_admin')->find($id);
 
 //        $entity = $this -> getNewEntity($entity);
 
-        if (!$entity) {
-            throw $this -> createNotFoundException('Unable to find User_admin entity.');
-        }
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find User_admin entity.');
+            }
 
-        $editForm = $this -> createForm(new User_adminType(), $entity);
+            $editForm = $this->createForm(new User_adminType(), $entity);
 //        $deleteForm = $this->createDeleteForm($id);
 
-        return $this -> render('FTRAdminBundle:Ftr_panel:adminprofile.html.twig', array(
-            'entity' => $entity,
-            'edit_form' => $editForm -> createView(),
+            return $this->render('FTRAdminBundle:Ftr_panel:adminprofile.html.twig', array(
+                'entity' => $entity,
+                'edit_form' => $editForm->createView(),
 //            'delete_form' => $deleteForm->createView(),
-        ));
+            ));
+        }else{
+            return $this -> redirect($this -> generateUrl('FTRAdminBundle_logout'));
+        }
     }
 
     /**
      * Edits an existing User_admin entity.
      *
      */
-    public function updateAction($id) {
-        $em = $this -> getDoctrine() -> getEntityManager();
+    public function updateAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
 
-        $entity = $em -> getRepository('FTRAdminBundle:User_admin') -> find($id);
+        $entity = $em->getRepository('FTRAdminBundle:User_admin')->find($id);
 
 
 //        $entity = $this -> getNewEntity($entity);
         if (!$entity) {
-            throw $this -> createNotFoundException('Unable to find User_admin entity.');
+            throw $this->createNotFoundException('Unable to find User_admin entity.');
         }
 
-        $editForm = $this -> createForm(new User_adminType(), $entity);
+        $editForm = $this->createForm(new User_adminType(), $entity);
         //$deleteForm = $this -> createDeleteForm($id);
 
-        $request = $this -> getRequest();
+        $request = $this->getRequest();
+        $editForm->bindRequest($request);
 
-        $editForm -> bindRequest($request);
+        $username = $entity->getUsername();
+        if($this->checkUser($username,$id) == 1){
+            if ($editForm->isValid()) {
+                $em->persist($entity);
+                $em->flush();
 
-        if ($editForm -> isValid()) {
-            $em -> persist($entity);
-            $em -> flush();
-
-            echo 'finish';
+                echo 'finish';
+                exit();
+                //return $this -> redirect($this -> generateUrl('user_admin_edit', array('id' => $id)));
+            }
+        }else{
+            echo 'fail';
             exit();
-            //return $this -> redirect($this -> generateUrl('user_admin_edit', array('id' => $id)));
         }
 
-        return $this -> render('FTRAdminBundle:Ftr_panel:adminprofile.html.twig', array(
+        return $this->render('FTRAdminBundle:Ftr_panel:adminprofile.html.twig', array(
             'entity' => $entity,
-            'edit_form' => $editForm -> createView(),
+            'edit_form' => $editForm->createView(),
 //            'delete_form' => $deleteForm -> createView(),
         ));
+
+    }
+
+    /**
+     * function check Username Admin .
+     *
+     */
+    private function checkUser($username,$id){
+        $conn = $this -> get('database_connection');
+        if (!$conn) { die("MySQL Connection error");
+        }
+        $sqlGetAdmin = "
+            SELECT
+              `username`
+            FROM
+              `user_admin`
+            WHERE id != $id AND username = '$username'
+        ";
+        try {
+            $entityAdmin= $conn -> fetchAll($sqlGetAdmin);
+
+            if(count($entityAdmin) == 0){
+                return true;
+            }
+            return false;
+        } catch (Exception $e) {
+            echo 'Caught exception: ', $e -> getMessage(), "\n";
+        }
+
     }
 
 }
--- src/FTR/AdminBundle/Controller/PanelController.php
+++ src/FTR/AdminBundle/Controller/PanelController.php
@@ -27,6 +27,7 @@ class PanelController extends Controller {
 					if (!empty($objSQL1)) {
 						$session = $this -> get('session');
 						$session -> set('username', $objSQL1[0]['username']);
+                        $session -> set('id', $objSQL1[0]['id']);
 						// echo "true";
 						// exit();
 						return $this -> redirect($this -> generateUrl('FTRAdminBundle_Dashboard'));
@@ -47,6 +48,7 @@ class PanelController extends Controller {
 		// exit();
 		$session = $this -> get('session');
 		$session -> set('username', '');
+        $session -> set('id', '');
 		return $this -> redirect($this -> generateUrl('FTRAdminBundle_panel'));
 	}
 
