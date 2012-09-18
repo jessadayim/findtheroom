607345580df070e455f84aa41996408dd6566532
suriya <suriya257_@hotmail.com>
2012-09-18 10:05:39 +0700

    แก้ไข forget password ขอรหัสผ่านอีกครั้ง จากล่าสุดต้อง 2 นาทีขึ้นไป

--- src/FTR/WebBundle/Controller/ForgetController.php
+++ src/FTR/WebBundle/Controller/ForgetController.php
@@ -21,23 +21,29 @@ class ForgetController extends Controller {
 		$em = $this -> getDoctrine() -> getEntityManager();
 		$conn = $this -> get('database_connection');
 
-		// $sql1 = "SELECT email,confirm_token FROM user_owner
-						// WHERE email = '$email' or username = '$email'";
-		// $objSQL1 = $conn -> fetchAll($sql1);
-		// echo $objSQL1[0]['confirm_token'];
-
-
 		if (!$conn) { die("MySQL Connection error");
 		}
 		try {
-			$sql1 = "SELECT id,email,confirm_token,deleted,firstname,lastname FROM user_owner
-						WHERE email = '$email' or username = '$email'";
+			$sql1 = "SELECT id,email,confirm_token,deleted,firstname,lastname,password_requested
+                     FROM user_owner
+				     WHERE email = '$email'
+				        OR username = '$email'";
 			$objSQL1 = $conn -> fetchAll($sql1);
+
             if($objSQL1[0]['deleted'] != 1){
                 $id = $objSQL1[0]['id'];
-
-                $time = date("Y-m-d : H:i:s", time());
-                $sqlLastRequest ="UPDATE user_owner SET password_requested = '$time' WHERE id= '$id'";
+                $lastForget = $objSQL1[0]['password_requested'];
+                $time = date("Y-m-d H:i:s", time());
+                if(!empty($lastForget)){
+                    $minute = (strtotime($time) - strtotime($lastForget)) / ( 60 );
+                    if( $minute < 2){
+                        echo '3';
+                        exit();
+                    }
+                }
+                $sqlLastRequest ="UPDATE user_owner
+                                  SET password_requested = '$time'
+                                  WHERE id= '$id'";
                 $conn->query($sqlLastRequest);
                 $host = "http://".$_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"];
                 $url = $this->get('router')->generate('TRWebBundle_change', array());
@@ -68,5 +74,8 @@ class ForgetController extends Controller {
 		}
 		exit();
 	}
+    private function dateDiff($strDateNow,$strLast){
+        return (strtotime($strDateNow) - strtotime($strLast)) / ( 60 );
+    }
 
 }
