e4e9a6e47efed25ab414960b76fadad0a4540aa5
Chukkapun <ruxchuk@gmail.com>
2012-09-20 17:42:20 +0700

    สร้างหน้า Nearly location

--- src/FTR/AdminBundle/Controller/Nearly_locationController.php
+++ src/FTR/AdminBundle/Controller/Nearly_locationController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use FTR\WebBundle\Entity\Nearly_location;
 use FTR\AdminBundle\Form\Nearly_locationType;
+use FTR\AdminBundle\Helper\Paginator;
+use FTR\AdminBundle\Helper\LoggerHelper;
 
 /**
  * Nearly_location controller.
@@ -19,36 +21,92 @@ class Nearly_locationController extends Controller
      */
     public function indexAction()
     {
-        $em = $this->getDoctrine()->getEntityManager();
+        $sqlGetEntity = "
+            SELECT
+              n.*,
+              t.type_name,
+              t.id AS t_id
+            FROM
+              `nearly_location` n
+              INNER JOIN `nearly_type` t
+                ON (n.nearly_type_id = t.id)
+            WHERE n.`deleted` = 0
+        ";
 
-        $entities = $em->getRepository('FTRWebBundle:Nearly_location')->findAll();
+        //get post
+        $getSelectPage = @$_GET['numPage'];
+        $getRecord = @$_GET['record'];
+        $getTextSearch = @$_GET['textSearch'];
+        $getOrderBy = @$_GET['orderBy'];
+        $getOrderByType = @$_GET['orderByType'];
 
-        return $this->render('FTRAdminBundle:Nearly_location:index.html.twig', array(
-            'entities' => $entities
-        ));
-    }
+        //set paging
+        $page = 1;
+        if (!empty($getSelectPage)){
+            $page = $getSelectPage;
+        }
+        $limit = 10;
+        $midRange = 5;
+        if(!empty($getRecord)){
+            $limit = $getRecord;
+        }else {
+            $getRecord = $limit;
+        }
+        $offset = $limit*$page-$limit;
 
-    /**
-     * Finds and displays a Nearly_location entity.
-     *
-     */
-    public function showAction($id)
-    {
-        $em = $this->getDoctrine()->getEntityManager();
-
-        $entity = $em->getRepository('FTRWebBundle:Nearly_location')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Nearly_location entity.');
+        if (empty($getOrderBy) && empty($getOrderByType)){
+            $getOrderBy = 'n.id';
+            $getOrderByType = 'asc';
+        }else{
+            $getOrderBy = htmlentities($getOrderBy);
+        }
+        if (!empty($getTextSearch) && $getTextSearch != ''){
+            $sqlGetEntity = "
+                $sqlGetEntity
+                AND n.id LIKE '%$getTextSearch%'
+                OR n.name LIKE '%$getTextSearch%'
+                OR n.address LIKE '%$getTextSearch%'
+                OR n.latitude LIKE '%$getTextSearch%'
+                OR n.longitude LIKE '%$getTextSearch%'
+                OR t.type_name LIKE '%$getTextSearch%'
+            ";
         }
 
-        $deleteForm = $this->createDeleteForm($id);
+        $sqlGetEntity = "
+            $sqlGetEntity
+            ORDER BY $getOrderBy $getOrderByType
+        ";
 
-        return $this->render('FTRAdminBundle:Nearly_location:show.html.twig', array(
-            'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),
+        //นับจำนวนที่มีทั้งหมด
+        $countList = count($this->getDataArray($sqlGetEntity));
 
+        //จำกัดการแสดงผล
+        $sqlGetEntity = "
+            $sqlGetEntity
+            LIMIT $offset, $limit
+        ";
+        $objResult = $this->getDataArray($sqlGetEntity);
+
+        $paginator = new Paginator($countList, $offset, $limit, $midRange);
+        return $this->render('FTRAdminBundle:Nearly_location:index.html.twig', array(
+            'entities'          => $objResult,
+            'paginator'	        => $paginator,
+            'countList'		    => $countList,
+            'limit' 	        => $limit,
+            'noPage'	        => $page,
+            'record'	        => $getRecord,
+            'textSearch'        => $getTextSearch,
+            'orderBy'           => $getOrderBy,
+            'orderByType'       => $getOrderByType
         ));
+
+//        $em = $this->getDoctrine()->getEntityManager();
+
+//        $entities = $em->getRepository('FTRWebBundle:Nearly_location')->findAll();
+//
+//        return $this->render('FTRAdminBundle:Nearly_location:index.html.twig', array(
+//            'entities' => $entities
+//        ));
     }
 
     /**
@@ -58,6 +116,10 @@ class Nearly_locationController extends Controller
     public function newAction()
     {
         $entity = new Nearly_location();
+
+        // เพิ่มชุด array ใช้ในการค้นหา
+        $entity = $this->getNewEntity($entity);
+
         $form   = $this->createForm(new Nearly_locationType(), $entity);
 
         return $this->render('FTRAdminBundle:Nearly_location:new.html.twig', array(
@@ -74,15 +136,32 @@ class Nearly_locationController extends Controller
     {
         $entity  = new Nearly_location();
         $request = $this->getRequest();
+
+        //Set ค่า deleted = 0
+        $entity->setDeleted(0);
+
+        // เพิ่มชุด array ใช้ในการค้นหา
+        $entity = $this->getNewEntity($entity);
+
         $form    = $this->createForm(new Nearly_locationType(), $entity);
         $form->bindRequest($request);
 
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getEntityManager();
+
+            //Check ชื่อซ้ำ
+            $getName = $entity->getName();
+            if (!$this->checkName($getName, "")){
+                echo "finish_comp";
+                exit();
+            }
+
             $em->persist($entity);
             $em->flush();
+            echo 'finish';
+            exit();
 
-            return $this->redirect($this->generateUrl('nearly_location_show', array('id' => $entity->getId())));
+//            return $this->redirect($this->generateUrl('nearly_location_show', array('id' => $entity->getId())));
             
         }
 
@@ -102,17 +181,28 @@ class Nearly_locationController extends Controller
 
         $entity = $em->getRepository('FTRWebBundle:Nearly_location')->find($id);
 
+        // เพิ่มชุด array ใช้ในการค้นหา
+        $entity = $this->getNewEntity($entity);
+
+        //Check post เปลี่ยน deleted เป็น 1
+        $getCheckPost = @$_POST['checkPost'];
+        if ($getCheckPost == "delete"){
+            $entity->setDeleted(1);
+            $em->persist($entity);
+            $em->flush();
+            echo 'finish';
+            exit();
+        }
+
         if (!$entity) {
             throw $this->createNotFoundException('Unable to find Nearly_location entity.');
         }
 
         $editForm = $this->createForm(new Nearly_locationType(), $entity);
-        $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('FTRAdminBundle:Nearly_location:edit.html.twig', array(
             'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+            'edit_form'   => $editForm->createView()
         ));
     }
 
@@ -126,62 +216,96 @@ class Nearly_locationController extends Controller
 
         $entity = $em->getRepository('FTRWebBundle:Nearly_location')->find($id);
 
+
+        // เพิ่มชุด array ใช้ในการค้นหา
+        $entity = $this->getNewEntity($entity);
+
+
+
         if (!$entity) {
             throw $this->createNotFoundException('Unable to find Nearly_location entity.');
         }
 
         $editForm   = $this->createForm(new Nearly_locationType(), $entity);
-        $deleteForm = $this->createDeleteForm($id);
 
         $request = $this->getRequest();
 
         $editForm->bindRequest($request);
-
         if ($editForm->isValid()) {
+
+            //Check ชื่อซ้ำกันหรือไม่
+            if(!$this->checkName($entity->getName(), "AND id != $id")){
+                echo "finish_comp";
+                exit();
+            }
+
             $em->persist($entity);
             $em->flush();
-
-            return $this->redirect($this->generateUrl('nearly_location_edit', array('id' => $id)));
+            echo 'finish';
+            exit();
+//            return $this->redirect($this->generateUrl('nearly_location_edit', array('id' => $id)));
         }
 
         return $this->render('FTRAdminBundle:Nearly_location:edit.html.twig', array(
             'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+            'edit_form'   => $editForm->createView()
         ));
     }
 
-    /**
-     * Deletes a Nearly_location entity.
-     *
+    /*
+     * Check ชื่อไม่ให้ซ้ำกัน
      */
-    public function deleteAction($id)
-    {
-        $form = $this->createDeleteForm($id);
-        $request = $this->getRequest();
-
-        $form->bindRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getEntityManager();
-            $entity = $em->getRepository('FTRWebBundle:Nearly_location')->find($id);
-
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Nearly_location entity.');
-            }
-
-            $em->remove($entity);
-            $em->flush();
+    private  function checkName($name, $sql){
+        $sqlCheck = "
+            SELECT
+              *
+            FROM
+              `nearly_location`
+            WHERE `name` = '$name'
+              AND `deleted` = 0
+              $sql
+        ";
+        $objCheck = $this->getDataArray($sqlCheck);
+        if (!empty($objCheck)){
+            return false;
         }
-
-        return $this->redirect($this->generateUrl('nearly_location'));
+        return true;
     }
 
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder(array('id' => $id))
-            ->add('id', 'hidden')
-            ->getForm()
-        ;
+    /*
+     * บันทึก log เกี่ยวกับการ insert, delete, update database
+     */
+    private function addLogger($message, $entity){
+        $logger = new LoggerHelper();
+        $newArray = $logger->objectToArray($entity);
+        $logger->addInfo($message, $newArray);
+    }
+
+    /*
+    * Run คำสั่ง Sql
+    * return array
+    */
+    private function getDataArray($sql){
+        $conn= $this->get('database_connection');
+        if(!$conn){ die("MySQL Connection error");}
+        try{
+            return $conn->fetchAll($sql);
+        } catch (Exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), "\n";
+        }
+        return array();
+    }
+
+    private function getNewEntity($Entity){
+        $sql = "
+            SELECT
+              *
+            FROM
+              `nearly_type`
+            WHERE `deleted` = 0
+        ";
+
+        $Entity->nearlyType = $this->getDataArray($sql);
+        return $Entity;
     }
 }
--- src/FTR/AdminBundle/Form/Nearly_locationType.php
+++ src/FTR/AdminBundle/Form/Nearly_locationType.php
@@ -9,13 +9,20 @@ class Nearly_locationType extends AbstractType
 {
     public function buildForm(FormBuilder $builder, array $options)
     {
+        $nearlyType[''] = '';
+        foreach ($options['data']->nearlyType as $key => $value){
+            $nearlyType[$value['id']] = $value['type_name'];
+        }
         $builder
-            ->add('nearly_type_id')
-            ->add('name')
-            ->add('address')
-            ->add('latitude')
-            ->add('longitude')
-            ->add('deleted')
+            ->add('nearly_type_id', 'choice', array(
+                'choices'   => $nearlyType,
+                'label'     => 'Type:'
+            ))
+            ->add('name', 'text', array('label' => 'Name:', "max_length" => 100))
+            ->add('address', 'textarea', array('label' => 'Address:', "max_length" => 100, 'required'  => false))
+            ->add('latitude', 'text', array('label' => 'Latitude:', 'max_length' => 25, 'required'  => false))
+            ->add('longitude', 'text', array('label' => 'Longitude:', 'max_length' => 25, 'required'  => false))
+//            ->add('deleted')
         ;
     }
 
